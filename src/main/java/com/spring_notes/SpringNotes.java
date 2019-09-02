638617c29197f7d1332a7a706cfdfdf963f2500b
Javi Gomez <rtomyj@gmail.com>
2019-09-02 13:07:31 -0500

    Added notes for Bean Scope and Bean Inheritence

--- src/main/java/com/spring_notes/SpringNotes.java
+++ src/main/java/com/spring_notes/SpringNotes.java
@@ -21,13 +21,51 @@ public class SpringNotes {
 
 		appContext = new ClassPathXmlApplicationContext(xml_configs);
 
-		Phone phone = (Phone) appContext.getBean("xml_config_phone");
+		Phone phone = (Phone) appContext.getBean("iphone_xs");
 		System.out.println(phone.toString());
 
 		PhonePurchase autowiredSetters = (PhonePurchase) appContext.getBean("phone_purchase");
 		System.out.println(autowiredSetters.toString());
 
+
 		LifeCycle lifeCycle = (LifeCycle) appContext.getBean("life_cycle");
+
+
+		/*
+			Difference between a singleton and prototype bean.
+
+			Singleton (default) - only one instance per bean.
+			Making 2+ will create 2+ objects with same reference.
+
+			Prototype - each bean is a new reference
+		*/
+		Phone singleton1 = (Phone) appContext.getBean("singleton"), singleton2 = (Phone) appContext.getBean("singleton");
+		Phone prototype1 = (Phone) appContext.getBean("prototype"), prototype2 = (Phone) appContext.getBean("prototype");
+
+		System.out.println();
+		System.out.println(String.format("Testing singleton on init: .equals() = %b", singleton1.equals(singleton2)));
+
+		singleton1.setName("Galaxy Note 10");
+		singleton1.setManufacture("Samsung");
+		System.out.println(String.format("Testing singleton bean after changing props: .equals() = %b"
+			, singleton1.equals(singleton2)));
+
+		System.out.println();
+		System.out.println(String.format("Testing prototype bean after init: .equals() = %b"
+			, singleton1.equals(singleton2)));
+
+		prototype1.setName("Galaxy Note 10");
+		prototype1.setManufacture("Samsung");
+		System.out.println(String.format("Testing prototype bean after changing props: .equals() = %b"
+			, prototype1.equals(prototype2)));
+
+
+		/*
+			Bean inheritance - parent bean specifies attributes all child beans will inherit (can be overridden)
+		*/
+		System.out.println();
+		Phone iphone = (Phone) appContext.getBean("iphone");
+		System.out.println(iphone.toString());
 
 		((ClassPathXmlApplicationContext) appContext).close();	// removes context with bean info for xml created beans
 	}
