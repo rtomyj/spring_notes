648b1e29c273408991f7dc64384895b364e1c5f4
Javi Gomez <rtomyj@gmail.com>
2019-09-13 21:29:25 -0500

    Removed xml created beans from annotation notes

--- src/main/java/com/spring_notes/SpringNotesAnnotation.java
+++ src/main/java/com/spring_notes/SpringNotesAnnotation.java
@@ -1,6 +1,7 @@
 package com.spring_notes;
 
 import com.spring_notes.component.ComputerComponent;
+import com.spring_notes.model.PhonePurchase;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -19,6 +20,11 @@ public class SpringNotesAnnotation
 	public void annotationConfigBaseExample()
 	{
 		appContext = new ClassPathXmlApplicationContext("beans/annotation_beans.xml");
+
+
+		// using bean name (first arg) and class being used (second arg)
+		PhonePurchase autowiredSetters = appContext.getBean("phone_purchase", PhonePurchase.class);
+		System.out.println(autowiredSetters.toString());
 
 		ComputerComponent computer = (ComputerComponent) appContext.getBean("sample");
 		System.out.println(computer.toString());
